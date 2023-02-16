74cdbd95a68ad3a319e27330f9ef2a93c871a6ba
Dheeraj-2205 <djsays124@gmail.com>
2023-02-16 14:05:19 +0530

    global css

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ const App = () => {
      <h1 className='heading-lg'>heading</h1>
      <button className='btn'>button</button>
      <button className='btn btn-black'>button</button>
+     <button className='btn btn-disable'>button</button>
+     <table>
+      <tr>
+        <th>dheeraj heading</th>
+        <th>dheeraj heading</th>
+        <th>dheeraj heading</th>
+      </tr>
+      <tr>
+        <td>one</td>
+        <td>one</td>
+        <td>one</td>
+      </tr>
+     </table>
+     <select className = 'f-controls'>
+      <option>1</option>
+      <option>1</option>
+      <option>1</option>
+      <option>1</option>
+      <option>1</option>
+     </select>
+     <input type = 'text' className='f-controls'/>
       <Header/>
       {/* <Index/> */}
       <Homepage/>
