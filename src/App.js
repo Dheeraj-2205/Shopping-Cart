028673475ccb207d0b04dd59bed49b4cf17fdee7
Dheeraj-2205 <djsays124@gmail.com>
2023-03-13 19:39:24 +0530

    Slider Part

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Footer from './components/footer/Footer'
 // import Index from './components/homepage/Index'
 import Homepage from './components/homepage/Index'
 // we using default we canv wasily change the name
+import MainSlider from './components/mainSlider/MainSlider'
 const App = () => {
   return (
     <>
     
      
       <Header/>
+      <MainSlider/>
       <Homepage/>
       <Footer/>
     </>
---
+++ src/components/mainSlider/MainSlider.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../mainSlider/mainSlider.css"
+const MainSlider = () => {
+  return (
+    <div className="dvMainSlider d-flex align-items-center">
+       <div className="container">
+        <div className="row">
+            <div className="col-md-6 text-center">
+                <img src="https://static.wixstatic.com/media/2c0034_5916d66c114c4a3fb055fd0fff15f402~mv2.png" 
+                    alt="" 
+                    className="img-fluid"
+                 />
+            </div>
+            <div className="col-md-6 text-center text-md-left">
+                <h1>Valencia Orange</h1>
+                <p className='py-2'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium fugit odio nam, ad numquam provident
+                    cupiditate sapiente magni architecto aspernatur. Facilis consectetur placeat rem. Sint mollitia corporis quo quidem non!
+                </p>
+                <button className='btn btn-black text-center'>Shop</button>
+            </div>
+        </div>
+        </div> 
+    </div>
+  )
+}
+
+export default MainSlider
