d70b3e44bfb5f4e8a3fa3c8bce731df38c3de5b2
Dheeraj-2205 <djsays124@gmail.com>
2023-03-22 12:20:33 +0530

    adding css slider part

--- src/components/mainSlider/MainSlider.jsx
+++ src/components/mainSlider/MainSlider.jsx
@@ -9,6 +9,7 @@ const MainSlider = () => {
                 <img src="https://static.wixstatic.com/media/2c0034_5916d66c114c4a3fb055fd0fff15f402~mv2.png" 
                     alt="" 
                     className="img-fluid"
+                    width={100}
                  />
             </div>
             <div className="col-md-6 text-center text-md-left">
